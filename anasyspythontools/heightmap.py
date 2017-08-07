015726139f324774bf6c08135c9e4e5706515b1e
Cody Schindler <admin@codyschindler.com>
2017-08-06 21:59:39 -0700

    SampleBase64 data now converted to numpy array. Basic display function

--- anasyspythontools/heightmap.py
+++ anasyspythontools/heightmap.py
@@ -6,6 +6,8 @@
 #
 #  This program is the property of Anasys Instruments, and may not be
 #  redistributed or modified without explict permission of the author.
+import numpy as np
+import matplotlib.pyplot as plt
 
 class HMElement(object):
     def __init__(self, nom="blank"):
@@ -17,6 +19,7 @@ class HeightMap():
     """A data structure for holding HeightMap data"""
     def __init__(self, hm):
         self._convert_tags(hm)
+        self._handle_img_data()
 
     def __dir__(self):
         return [x for x in dir(self) if x[0]!='_']
@@ -46,3 +49,31 @@ class HeightMap():
                 setattr(element_obj, child.tag, rr)
             #Return the object containing all children and attributes
             return element_obj
+
+    def _handle_img_data(self):
+        """Converts bytestring into numpy array of correct size and shape"""
+        if self.SampleBase64 == {}:
+            return
+        #Format structure is Xres * Yres floating points (returns a tuple)
+        data_format = "f" * self.Resolution.X * self.Resolution.Y
+        #Re-encode to bytes from string, then decode bytes using base64
+        decoded = codecs.decode(self.SampleBase64.encode(), 'base64')
+        #Unpack the data
+        data = struct.unpack(data_format, decoded)
+        #Reshape the data as a numpy array and save over the string
+        self.SampleBase64 = np.array(data).reshape(self.Resolution.X, self.Resolution.Y)
+
+    def show(self):
+        if self.SampleBase64 == {}:
+            return
+        axes = [0, self.Size.X, 0, self.Size.Y]
+        _max = np.absolute(self.SampleBase64).max()
+        rmax = _max
+        if (_max //5) % 5 != 0:
+            rmax = (_max // 5)*5 + 5
+        plt.imshow(self.SampleBase64, cmap='gray', interpolation='none', extent=axes, vmin=-rmax, vmax=rmax)
+        plt.xlabel('μm')
+        plt.ylabel('μm')
+        # plt.title(self.Label)
+        plt.colorbar().set_label(self.UnitPrefix + self.Units)
+        plt.show()
